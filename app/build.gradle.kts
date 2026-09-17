30c887ae02573f5ae8bb7813b5834bec8358601d
rami28091995-commits <rami28091995@gmail.com>
2026-09-17 20:32:26 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,29 +1,22 @@
 plugins {
-    id("com.android.application") version "8.2.2"
-    id("org.jetbrains.kotlin.android") version "1.9.10"
+    id 'com.android.application'
 }
 android {
-    namespace = "com.dalili.majid"
-    compileSdk = 34
+    namespace 'com.dalili.majid'
+    compileSdk 34
     defaultConfig {
-        applicationId = "com.dalili.majid"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        applicationId "com.dalili.majid"
+        minSdk 21
+        targetSdk 34
+        versionCode 1
+        versionName "1.0"
     }
     buildTypes {
-        release {
-            isMinifyEnabled = false
+        debug {
+            minifyEnabled false
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation 'androidx.appcompat:appcompat:1.6.1'
 }
